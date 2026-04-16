7caeb7f728c8c6374bbc9f60b9fb50026c4fc0a3
Eline De Buyser <elinedebuyser@Smexmachine-2.local>
2026-04-16 17:54:50 +0700

    web: fix hero video mobile sizing & use compressed video

--- apps/web/components/HeroVideo.tsx
+++ apps/web/components/HeroVideo.tsx
@@ -1,27 +1,46 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from "./HeroVideo.module.css";
 
-export default function HeroVideo() {
+export default function HeroVideo() { 
+    const mobileVideo = { src: "/images/hero-video-mobile.mp4", height: 406, width: 720 };
+    const desktopVideo = { src: "/images/hero-video.mp4", height: 1080, width: 1920 };
+
+    const [video, setVideo] = useState(mobileVideo);
+
+    useEffect(() => {
+        const isMobile = window.innerWidth < 768;
+        setVideo(isMobile ? mobileVideo : desktopVideo);
+    }, []);
+
+    // todo: fallback image must have same aspect ratio as video!
+
     return (
         <div className={styles.heroContainer}>
-
             <video autoPlay loop muted playsInline
                 className={styles.videoElement}
                 poster="/images/hero-video-fallback.avif"
-                width="1920" height="1080">
-                <source src="/images/new-hero-video-720.mp4" type="video/mp4" />
+                width={video.width} height={video.height}>
+                <source 
+                    src={video.src} 
+                    type="video/mp4"
+                />  
             </video>
 
-            <div className={styles.videoElement}>
-                <Image src="/images/hero-image-mobile-sharp.avif" alt="Harmony Hill Retreat" width="800" height="1200" fetchPriority="high" />
-            </div>
-
             <div className={styles.overlay}>
-                <Image src="/images/logo-white-transparent-background.avif" alt="Harmony Hill Logo" width="540" height="540"
-                    fetchPriority="high" className={styles.logo} />
-                <h1 className={styles.slogan}>Two Private Villas in the heart of Bali <br /> 100% Vegan</h1>
+                <Image 
+                    src="/images/logo-white-transparent-background.avif" 
+                    alt="Harmony Hill Logo" 
+                    width="540" height="540"
+                    fetchPriority="high" 
+                    className={styles.logo} 
+                />
+
+                <h1 className={styles.slogan}>Two Private Villas in the heart of Bali <br />
+                    100% Vegan
+                </h1>
             </div>
         </div>
     );
